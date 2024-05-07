5e529319bd69e6700c50e8d55236da13dc539b0e
MMarQs <108202305+MMarQs@users.noreply.github.com>
2024-05-07 21:08:23 +0100

    Pagination/Explore more

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import './css/ShopCategory.css'
 import { ShopContext } from '../Context/ShopContext'
 import dropdown_icon from '../Components/Assets/dropdown_icon.png'
@@ -6,32 +6,40 @@ import Item from '../Components/Item/Item'
 
 export const ShopCategory = (props) => {
   const {all_product} = useContext(ShopContext);
-
-  function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  }
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 16;
+  const totalPages = Math.ceil(all_product.length / itemsPerPage);
+  
+  const loadPage = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   return (
     <div className='shop-category'>
       <div className='shopcategory-indexSort'>
         <p>
-          <span>Showing 1-12</span> out of 36 products
+          <span>Showing 1-16</span> out of 36 products
         </p>
         <div className='shopcategory-sort'>
           Sort by <img src={dropdown_icon} alt="" />
         </div>
       </div>
       <div className='shopcategory-products'>
-        {shuffleArray(all_product).map((item, i) => {
+        {all_product.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage).map((item, i) => {
           return <Item key={i} id={item.id} name={item.name} image={item.image} streaming_price={item.streaming_price} streaming_old_price={item.streaming_old_price}/>;
         })}
       </div> 
-      <div className='shopcategory-loadmore'>
-        Explore More
+
+      <div className='shopcategory-pagination'>
+        {[...Array(totalPages).keys()].map((page) => (
+          <button 
+            key={page} 
+            onClick={() => loadPage(page + 1)}
+            className={currentPage === page + 1 ? 'active' : ''}
+          >
+            {page + 1}
+          </button>
+        ))}
       </div>
     </div>
   )
