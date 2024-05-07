f85b680f8fb8238b0ec7cfbdd5210140c9b4d550
MMarQs <108202305+MMarQs@users.noreply.github.com>
2024-05-07 12:23:10 +0100

    Data update

--- frontend/src/Components/Assets/data.js
+++ frontend/src/Components/Assets/data.js
@@ -13,6 +13,10 @@ let data_product = [
     image: p1_img,
     new_price: 4.99,
     old_price: 9.99,
+    movie_description: "Armed with only one word, Tenet, and fighting for the survival of the entire world, a Protagonist journeys through a twilight world of international espionage on a mission that will unfold in something beyond real time.",
+    movie_stars: "John David Washington, Robert Pattinson, Elizabeth Debicki",
+    movie_director: "Christopher Nolan",
+    movie_runtime: "2h 30min",
   },
   {
     id: 2,
@@ -23,6 +27,10 @@ let data_product = [
     image: p2_img,
     new_price: 4.99,
     old_price: 9.99,
+    movie_description: "Diana must contend with a work colleague and businessman, whose desire for extreme wealth sends the world down a path of destruction, after an ancient artifact that grants wishes goes missing.",
+    movie_stars: "Gal Gadot, Chris Pine, Kristen Wiig",
+    movie_director: "Patty Jenkins",
+    movie_runtime: "2h 31min",
   },
   {
     id: 3,
@@ -33,6 +41,10 @@ let data_product = [
     image: p3_img,
     new_price: 3.49,
     old_price: 9.99,
+    movie_description: "After landing the gig of a lifetime, a New York jazz pianist suddenly finds himself trapped in a strange land between Earth and the afterlife.",
+    movie_stars: "Jamie Foxx, Tina Fey, Graham Norton",
+    movie_director: "Pete Docter, Kemp Powers",
+    movie_runtime: "1h 40min",
   },
   {
     id: 4,
@@ -43,6 +55,10 @@ let data_product = [
     image: p4_img,
     new_price: 4.99,
     old_price: 9.99,
+    movie_description: "A woman in her sixties, after losing everything in the Great Recession, embarks on a journey through the American West, living as a van-dwelling modern-day nomad.",
+    movie_stars: "Frances McDormand, David Strathairn, Linda May",
+    movie_director: "Chloé Zhao",
+    movie_runtime: "1h 47min",
   },
 ];
 
