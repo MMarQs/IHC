e67959f27337e39a6cb03e6470c013a3eb72ff52
Gonçalo Oliveira <goncalooliveira04@ua.pt>
2024-05-07 10:57:56 +0100

    add searchBar

--- frontend/src/Components/SearchBar/SearchBar.jsx
+++ frontend/src/Components/SearchBar/SearchBar.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './SearchBar.css'
 
 export const SearchBar = () => {
+  const [input, setInput] = useState("")
   return (
-    <div className='searchbar'>
-
+    <div className="input-wrapper"> 
+      <input placeholder="Type to search..." value={input} onChange={(e) => setInput(e.target.value)}/> 
     </div>
   )
 }
